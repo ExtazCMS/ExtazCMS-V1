d4c116a49cae1c748a05128599c1030f24ea0820
VectorFR <venard.paul@gmail.com>
2017-03-20 20:43:53 +0100

    Banned players are now redirect to home page with a pop-up message when they try to acces to : chat & votes

--- app/Controller/ChatController.php
+++ app/Controller/ChatController.php
@@ -5,7 +5,16 @@ Class ChatController extends AppController {
     public function index() {
         // Si le chat est activée
         if($this->config['use_igchat'] == 1) {
-        	$ban = ($this->Auth->user('ban'));
+            if($this->Auth->user()) {
+                $ban = ($this->Auth->user('ban'));
+                if($ban == 1){
+                    $this->Session->setFlash('Les joueurs bannis n\'ont pas accès au chat', 'error');
+                    return $this->redirect(['controller' => 'posts', 'action' => 'index']);
+                }
+            }
+            else {
+                $ban = 0;
+            }
         } // Si le chat est désactivée
         else {
             throw new NotFoundException();
