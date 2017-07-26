cf73bfebfc5fe35a5017bdb172f830afca9bac77
Spiikesan <Spiikesan@users.noreply.github.com>
2017-07-26 17:03:06 +0200

    Correction of a previous breaking change
    Tickets responses are not saved into database

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -594,6 +594,8 @@ class PagesController extends AppController {
 	public function answer_ticket($id = null){
 		if($this->Auth->user()){
 			if($this->request->is('post')){
+			$message = nl2br(htmlspecialchars($this->request->data['Pages']['message']));
+			if(!empty($message)){
 				if(!empty($this->request->data['Pages']['message'])){
 					$ticket = $this->Support->find('first', ['conditions' => ['Support.id' => $this->request->data['Pages']['id']]]);
 					$ticket_owner = $this->User->find('first', ['conditions' => ['User.username' => $ticket['User']['username']]]);
@@ -611,7 +613,6 @@ class PagesController extends AppController {
 							// 	$Email->subject('['.$this->config['name_server'].'] Support, nouvelle réponse à votre ticket #'.$ticket['Support']['id'].'');
 							// 	$Email->send('Retrouvez cette nouvelle réponse ici : http://'.$_SERVER['HTTP_HOST'].$this->webroot.'tickets/'.$ticket['Support']['id']);
 							// }
-							$message = nl2br(htmlspecialchars($message));
 							$this->supportComments->create;
 							$this->supportComments->saveField('ticket_id', $this->request->data['Pages']['id']);
 							$this->supportComments->saveField('user_id', $this->Auth->user('id'));
